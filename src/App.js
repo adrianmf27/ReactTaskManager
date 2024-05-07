b43a84994a80b57c3e4cd6975b1f151320294bb2
Adrián Martínez Fuentes <UO295454@uniovi.es>
2024-05-07 18:19:25 +0200

    Updated React Task Manager files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import { useState, useEffect } from 'react';
+import { useState, useRef } from 'react';
 import './App.css';
 
 function App() {
+
+  // List variable to save the different tasks
   let [task, setTask] = useState(
     [
       {
@@ -16,9 +18,62 @@ function App() {
       }
     ]
   )
+
+  // Variables to save input values
+  let nameRef = useRef("")
+  let placeRef = useRef("")
+  let priorityRef = useRef(0)
+
+  let formRef = useRef()
+
+  // Adding the task to the list
+  let addTaskWithReferences = () => {
+    let newTask = {
+      name : nameRef.current.value,
+      place : placeRef.current.value,
+      priority : priorityRef.current.value,
+    }
+
+    setTask([...task, newTask])
+
+    nameRef.current.value = ""
+    placeRef.current.value = ""
+    priorityRef.current.value = 0
+  }
+
+
+  // Adding the task to the list with form
+  let addTaskWithFormReferences = () => {
+    let formData = new FormData(formRef.current)
+
+    let newTask = {
+      name : formData.get("name"),
+      place : formData.get("place"),
+      priority : formData.get("priority")
+    }
+    
+    setTask([...task, newTask])
+  }
+
   return (
     <>
       <div>
+        <h2>Add a task</h2>
+        <input ref = {nameRef} type="text" placeholder='name'></input>
+        <input ref = {placeRef} type="text" placeholder='place'></input>
+        <input ref = {priorityRef} type="number" placeholder='priority'></input>
+        <button onClick={addTaskWithReferences}>Add the task</button>
+
+
+        <h2>Add a task with a Form Ref</h2>
+        <form ref={formRef}>
+          <input name="name" type="text" placeholder='name'></input>
+          <input name="place" type="text" placeholder='place'></input>
+          <input name="priority" type="number" placeholder='priority'></input>
+        </form>
+        <button onClick={addTaskWithFormReferences}>Add the task</button>
+
+
         <ul>
           { task.map (t =>
             <li>
